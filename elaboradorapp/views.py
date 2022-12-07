de7c16d615caeb42f9013d33feb1f5d3c842a522
Jean Carlos <jeancarllos99@outlook.com>
2022-12-06 23:10:25 -0300

    att

--- elaboradorapp/views.py
+++ elaboradorapp/views.py
@@ -1,16 +1,27 @@
 from django.views.generic import ListView, View
-from elaboradorapp.utils import GeraPDFMixin
+# from elaboradorapp.utils import GeraPDFMixin
 from .models import Question, Disciplina, Conteudo
-from django.db.models import Q
+
 
 class ElaboradorApp(ListView):
     model = Question
     template_name = 'elaboradorapp/index.html'
 
     def get_context_data(self, **kwargs):
+        global disciplina_escolhida
+        global conteudo_escolhido
+        global serie_escolhida
+        global dificuldade_escolhida
+       
         context = super(ElaboradorApp, self).get_context_data(**kwargs)
         context['disciplinas'] = Disciplina.objects.all()
         context['conteudos'] = Conteudo.objects.all()
+
+        disciplina_escolhida = self.request.GET.get('disciplina_value')
+        conteudo_escolhido = self.request.GET.get('conteudo_value')
+        serie_escolhida = self.request.GET.get('serie_value')
+        dificuldade_escolhida = self.request.GET.get('dificuldade_value')
+
         return context
 
 class ListarQuestoes(ListView):
@@ -20,22 +31,22 @@ class ListarQuestoes(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         questoes = Question.objects.all()
+        conteudo = Conteudo.objects.filter(name=conteudo_escolhido)
+        disciplina = Disciplina.objects.filter(name=disciplina_escolhida)
 
-        disciplina = self.request.GET.get('disciplina_value')
-        conteudo = self.request.GET.get('conteudo_value')
-        serie = self.request.GET.get('serie_value')
-        dificuldade = self.request.GET.get('dificuldade_value')
+        conteudo = conteudo[0].pk
+        disciplina = disciplina[0].pk
 
         if disciplina:
-            questoes = Question.objects.filter(Q(disciplina__icontains=disciplina) & Q(conteudo__icontains=conteudo) & Q(serie__icontains=serie) & Q(dificuldade__icontains=dificuldade))
-        
+            questoes = Question.objects.filter(disciplina=disciplina, conteudo=conteudo, serie=serie_escolhida, dificuldade=dificuldade_escolhida)
+
         context.update({
             'questoes': questoes,
         })
         return context
 
 
-class ListarQuestoesPDF(View, GeraPDFMixin):
-    def get(self, request, *args, **kwargs):
-        questoes = Question.objects.all()
+# class ListarQuestoesPDF(View, GeraPDFMixin):
+#     def get(self, request, *args, **kwargs):
+#         questoes = Question.objects.all()
         
