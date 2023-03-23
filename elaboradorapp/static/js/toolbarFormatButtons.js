edcb45429ad9063099f3388540236308c2e4382b
Jean Carlos <jeancarllos99@outlook.com>
2023-03-22 22:13:01 -0300

    Nova atualização

--- elaboradorapp/static/js/toolbarFormatButtons.js
+++ elaboradorapp/static/js/toolbarFormatButtons.js
@@ -208,3 +208,95 @@ function formataFontColor() {
         }
     }
 }
+
+function reorganizarNumeracao() {
+    // obter todos os elementos com a classe "questao"
+    let questoes = document.querySelectorAll("#questao");
+
+    // obter todos os elementos com a classe "questão-{{ forloop.counter }} div-questoes"
+    let divQuestoes = document.querySelectorAll(".div-questoes");
+
+    // atualizar a numeração de cada elemento
+    for (let i = 0; i < questoes.length; i++) {
+        // encontrar o elemento "strong" dentro da questão
+        let strong = questoes[i].querySelector("strong");
+
+        // atualizar o número da questão
+        let numQuestao = i + 1;
+        strong.innerHTML = numQuestao + ". ";
+
+        // atualizar a classe da div-questoes com a nova numeração
+        let classeAtual = divQuestoes[i].className;
+        let novaClasse = classeAtual.replace(
+            /questao-\d+/,
+            `questao-${numQuestao}`
+        );
+        divQuestoes[i].className = novaClasse;
+
+        // atualizar a numeração recursivamente em todas as sub-questões
+        reorganizarSubNumeracao(questoes[i], numQuestao);
+    }
+
+    // selecionar todas as linhas da tabela de gabarito
+    let linhasGabarito = document.querySelectorAll('.tr-questao-gabarito');
+
+    // iterar sobre as linhas e atualizar as classes
+    for (let i = 0; i < linhasGabarito.length; i++) {
+        // obter o número da questão
+        let numQuestao = i + 1;
+
+        // atualizar a classe da linha com a nova numeração
+        let classeAtual = linhasGabarito[i].className;
+        let novaClasse = classeAtual.replace(
+            /tr-questao-\d+/,
+            `tr-questao-${numQuestao}`
+        );
+        linhasGabarito[i].className = novaClasse;
+    }
+
+
+    // atualizar a numeração das questões do gabarito
+    let trQuestoes = document.querySelectorAll(".tr-questao-gabarito");
+    for (let i = 0; i < trQuestoes.length; i++) {
+        let numeroDaQuestao = trQuestoes[i].querySelector(".numero-da-questao");
+        numeroDaQuestao.innerHTML = i + 1;
+    }
+}
+
+function reorganizarSubNumeracao(parentElement, index) {
+    // obter todos os elementos com a classe "questao"
+    let subQuestoes = parentElement.querySelectorAll("#questao");
+
+    // atualizar a numeração de cada sub-questão
+    for (let i = 0; i < subQuestoes.length; i++) {
+        // encontrar o elemento "strong" dentro da sub-questão
+        let strong = subQuestoes[i].querySelector("strong");
+
+        // atualizar o número da sub-questão
+        strong.innerHTML = index + "." + (i + 1);
+
+        // atualizar o número da sub-questão no gabarito
+        let gabaritoNum = document.querySelector(
+            `.tr-questao-${parentElement.dataset.id} .gabarito-num-${i + 1}`
+        );
+        gabaritoNum.innerHTML = index + "." + (i + 1);
+
+        // atualizar a numeração recursivamente em todas as sub-questões
+        reorganizarSubNumeracao(subQuestoes[i], index + "." + (i + 1));
+    }
+}
+
+var trashButton = document.querySelector(".trash");
+
+trashButton.addEventListener("click", function () {
+    let numberQuestionDelete = document.querySelector(".number-question-delete").value;
+    let question = document.querySelector(`.questao-${numberQuestionDelete}`);
+    let trQuestion = document.querySelector(`.tr-questao-${numberQuestionDelete}`);
+    // pedir uma confirmação
+    let confirmacao = confirm("Tem certeza que deseja excluir esta questão?");
+    if (confirmacao && question) {
+        question.remove();
+        trQuestion.remove();
+        reorganizarNumeracao();
+    }
+});
